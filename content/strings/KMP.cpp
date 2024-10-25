dd78b99fe50bbcd2047220f21fab01cc27088c4c
PBBx0 <ravv.original@gmail.com>
2024-10-25 15:55:55 +0300

    add KMP

---
+++ content/strings/KMP.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: Vladimir Ragulin
+ * Description: Prefix-function and Z-function
+ * Time: O(n)
+ */
+
+vector<int> Pi(const string& s) {
+	vector<int> p(sz(s));
+	for (int i = 1; i < sz(s); ++i) {
+		int g = p[i - 1];
+		while (g && s[i] != s[g]) g = p[g - 1];
+		p[i] = g + (s[i] == s[g]);
+	}
+	return p;
+}
+
+vector<int> Zf(const string& s) {
+	vector<int> z(sz(s));
+	int l = -1, r = -1;
+	for (int i = 1; i < sz(s); ++i) {
+		z[i] = i >= r ? 0 : min(r - i, z[i - l]);
+		while (i + z[i] < sz(s) && s[i + z[i]] == s[z[i]]) ++z[i];
+		if (i + z[i] > r) l = i, r = i + z[i];
+	}
+	return z;
+}
